db03acc2bc7a2aa3b362597978706da171cd1dca
Kevin Figuera <kevin.figuera@techtool.io>
2023-02-26 22:42:34 -0400

    first commit

---
+++ src/Controller/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/')]
+    public function home(Request $request): Response
+    {
+        $search = $request->get('search');
+
+        return $this->render('home.html.twig', compact('search'));
+    }
+}
