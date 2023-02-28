b443bb1a1f328cc2c1f3abed09e86b5fda2f37e1
Kevin Figuera <kevin.figuera@techtool.io>
2023-02-27 20:17:02 -0400

    Estructura inicial

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
     #[Route('/')]
-    public function home(Request $request): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
-        $search = $request->get('search');
-        return $this->render('home.html.twig', compact('search'));
+        $comments = $this->getComments($entityManager);
+        return $this->render('home.html.twig', compact('comments'));
+    }
+
+    private function getComments(EntityManagerInterface $entityManager)
+    {
+        return $entityManager->getRepository(Comment::class)->findBy([], [
+            'id' => 'DESC'
+        ]);
     }
 }
