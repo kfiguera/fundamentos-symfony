d9a668f635ed1f1a9e89853189f634af3c974273
Kevin Figuera <kevin.figuera@techtool.io>
2023-02-28 22:18:45 -0400

    Form

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,18 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/')]
-    public function home(EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'home')]
+    public function home(EntityManagerInterface $entityManager, Request $request): Response
     {
+        $form = $this->createForm(CommentType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+
         $comments = $this->getComments($entityManager);
-        return $this->render('home.html.twig', compact('comments'));
+        return $this->render('home.html.twig', [
+            'comments' => $comments,
+            'form' => $form->createView()
+        ]);
     }
 
     private function getComments(EntityManagerInterface $entityManager)
